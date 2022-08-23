4bbaccedda4074f8e554926523379bd616d6f9b2
mariodlrosario <75270826+mariodlrosario@users.noreply.github.com>
2022-08-23 16:22:30 +0100

    Update gen_anchors.py

--- gen_anchors.py
+++ gen_anchors.py
@@ -1,10 +1,8 @@
-import sys
-sys.path.append("..")
-from keras_yolov2.preprocessing import parse_annotation_xml, parse_annotation_csv
-from keras_yolov2.utils import import_feature_extractor
-import numpy as np
-import argparse
 import random
+import argparse
+import numpy as np
+
+from preprocessing import parse_annotation
 import json
 
 argparser = argparse.ArgumentParser()
@@ -12,18 +10,16 @@ argparser = argparse.ArgumentParser()
 argparser.add_argument(
     '-c',
     '--conf',
-    default='../config.json',
+    default='config.json',
     help='path to configuration file')
 
 argparser.add_argument(
     '-a',
     '--anchors',
     default=5,
-    help='number of anchors to use',
-    type=int)
+    help='number of anchors to use')
 
-
-def iou(ann, centroids):
+def IOU(ann, centroids):
     w, h = ann
     similarities = []
 
@@ -36,21 +32,20 @@ def iou(ann, centroids):
             similarity = w*c_h/(w*h + (c_w-w)*c_h)
         elif c_w <= w and c_h >= h:
             similarity = c_w*h/(w*h + c_w*(c_h-h))
-        else:  # means both w,h are bigger than c_w and c_h respectively
+        else: #means both w,h are bigger than c_w and c_h respectively
             similarity = (c_w*c_h)/(w*h)
-        similarities.append(similarity)  # will become (k,) shape
+        similarities.append(similarity) # will become (k,) shape
 
     return np.array(similarities)
 
+def avg_IOU(anns, centroids):
+    n,d = anns.shape
+    sum = 0.
 
-def avg_iou(anns, centroids):
-    n, d = anns.shape
-    _sum = 0.
     for i in range(anns.shape[0]):
-        _sum += max(iou(anns[i], centroids))
+        sum+= max(IOU(anns[i], centroids))
 
-    return _sum/n
-
+    return sum/n
 
 def print_anchors(centroids):
     anchors = centroids.copy()
@@ -60,22 +55,22 @@ def print_anchors(centroids):
 
     r = "anchors: ["
     for i in sorted_indices[:-1]:
-        r += '%0.5f,%0.5f, ' % (anchors[i, 0], anchors[i, 1])
+        r += '%0.2f,%0.2f, ' % (anchors[i,0], anchors[i,1])
 
-    # there should not be comma after last anchor, that's why
-    r += '%0.5f,%0.5f' % (anchors[sorted_indices[-1:], 0], anchors[sorted_indices[-1:], 1])
+    #there should not be comma after last anchor, that's why
+    r += '%0.2f,%0.2f' % (anchors[sorted_indices[-1:],0], anchors[sorted_indices[-1:],1])
     r += "]"
 
     print(r)
 
-
 def run_kmeans(ann_dims, anchor_num):
     ann_num = ann_dims.shape[0]
+    iterations = 0
     prev_assignments = np.ones(ann_num)*(-1)
     iteration = 0
     old_distances = np.zeros((ann_num, anchor_num))
 
-    indices = [random.randrange(ann_dims.shape[0]) for _ in range(anchor_num)]
+    indices = [random.randrange(ann_dims.shape[0]) for i in range(anchor_num)]
     centroids = ann_dims[indices]
     anchor_dim = ann_dims.shape[1]
 
@@ -83,74 +78,60 @@ def run_kmeans(ann_dims, anchor_num):
         distances = []
         iteration += 1
         for i in range(ann_num):
-            d = 1 - iou(ann_dims[i], centroids)
+            d = 1 - IOU(ann_dims[i], centroids)
             distances.append(d)
-        distances = np.array(distances)  # distances.shape = (ann_num, anchor_num)
+        distances = np.array(distances) # distances.shape = (ann_num, anchor_num)
 
         print("iteration {}: dists = {}".format(iteration, np.sum(np.abs(old_distances-distances))))
 
-        # assign samples to centroids
-        assignments = np.argmin(distances, axis=1)
+        #assign samples to centroids
+        assignments = np.argmin(distances,axis=1)
 
-        if (assignments == prev_assignments).all():
+        if (assignments == prev_assignments).all() :
             return centroids
 
-        # calculate new centroids
-        centroid_sums = np.zeros((anchor_num, anchor_dim), np.float)
+        #calculate new centroids
+        centroid_sums=np.zeros((anchor_num, anchor_dim), np.float)
         for i in range(ann_num):
-            centroid_sums[assignments[i]] += ann_dims[i]
+            centroid_sums[assignments[i]]+=ann_dims[i]
         for j in range(anchor_num):
-            centroids[j] = centroid_sums[j]/(np.sum(assignments == j) + 1e-6)
+            centroids[j] = centroid_sums[j]/(np.sum(assignments==j) + 1e-6)
 
         prev_assignments = assignments.copy()
         old_distances = distances.copy()
 
-
-def main(args):
+def main(argv):
     config_path = args.conf
     num_anchors = args.anchors
 
     with open(config_path) as config_buffer:
         config = json.loads(config_buffer.read())
 
-    if config['parser_annotation_type'] == 'xml':
-        # parse annotations of the training set
-        train_imgs, train_labels = parse_annotation_xml(config['train']['train_annot_folder'], 
-                                                        config['train']['train_image_folder'],
-                                                        config['model']['labels'])
-    elif config['parser_annotation_type'] == 'csv':
-        # parse annotations of the training set
-        train_imgs, train_labels = parse_annotation_csv(config['train']['train_csv_file'],
-                                                        config['model']['labels'],
-                                                        config['train']['train_csv_base_path'])
+    train_imgs, train_labels = parse_annotation(config['train']['train_annot_folder'],
+                                                config['train']['train_image_folder'],
+                                                config['model']['labels'])
 
-    input_size = (config['model']['input_size_h'], config['model']['input_size_w'], 3)
-    feature_extractor = import_feature_extractor(config['model']['backend'], input_size)
-    # grid_w should be the number of grid cells, not the size of a single grid cell
-    #grid_w = config['model']['input_size_w']/feature_extractor.get_output_shape()[1]
-    #grid_h = config['model']['input_size_h']/feature_extractor.get_output_shape()[0]
-    grid_w = feature_extractor.get_output_shape()[1]
-    grid_h = feature_extractor.get_output_shape()[0]
+    grid_w = config['model']['input_size']/32
+    grid_h = config['model']['input_size']/32
 
     # run k_mean to find the anchors
     annotation_dims = []
     for image in train_imgs:
-        cell_w = float(image['width'])/grid_w
-        cell_h = float(image['height'])/grid_h
+        cell_w = image['width']/grid_w
+        cell_h = image['height']/grid_h
 
         for obj in image['object']:
             relative_w = (float(obj['xmax']) - float(obj['xmin']))/cell_w
             relatice_h = (float(obj["ymax"]) - float(obj['ymin']))/cell_h
-            annotation_dims.append(tuple(map(float, (relative_w, relatice_h))))
+            annotation_dims.append(tuple(map(float, (relative_w,relatice_h))))
 
     annotation_dims = np.array(annotation_dims)
     centroids = run_kmeans(annotation_dims, num_anchors)
 
     # write anchors to file
-    print('\naverage IOU for', num_anchors, 'anchors:', '%0.2f' % avg_iou(annotation_dims, centroids))
+    print('\naverage IOU for', num_anchors, 'anchors:', '%0.2f' % avg_IOU(annotation_dims, centroids))
     print_anchors(centroids)
 
-
 if __name__ == '__main__':
-    _args = argparser.parse_args()
-    main(_args)
+    args = argparser.parse_args()
+    main(args)
